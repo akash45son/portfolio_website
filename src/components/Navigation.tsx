515d1f7d98fd5eab7c162d97eaaa1c544ce78d02
Akash Sonawane <1985swatid@gmail.com>
2026-02-28 23:41:38 +0530

    added a image to my profile icon

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -38,9 +38,17 @@ export default function Navigation() {
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          <a href="#home" className="text-xl font-bold text-white" onClick={(e) => scrollToSection(e, '#home')}>
-            AS
+          
+          <div className="flex items-center space-x-4">
+              <a href="#home" className="text-xl font-bold text-white" onClick={(e) => scrollToSection(e, '#home')}>
           </a>
+          <img  
+          src="./src/assets/images/pro.jpg" 
+          alt="Profile" 
+          className="w-10 h-10 rounded-full object-cover border-2 border-gray-700 shadow-md "/>
+          </div>
+            
+          
 
           <div className="hidden md:flex space-x-8">
             {navLinks.map((link) => (
